3fc4a5cc04dde6575990810b1f8aaf88754ac76a
unknown <exekuta@gmail.com>
2021-11-05 01:22:00 +0300

    feat: add solution for 1 and 2 task

---
+++ 02-write-file/index.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const fs = require('fs');
+const { stdin, stdout } = process;
+
+// const readline = require('readline');
+// const rl = readline.createInterface({ stdin, stdout });
+
+const filePath = path.join(__dirname, 'destination.txt');
+const writeableStream = fs.createWriteStream(filePath);
+
+stdout.write('\nWhat is your name?\n');
+stdin.on('data', (data) => {
+    const trimData = data.toString().trim();
+    if (trimData == 'exit') {
+    process.exit();
+  } else {
+    writeableStream.write(data);
+    stdout.write('\nNeed mo-o-o-ore information!\n');
+    stdout.write('CTRL + C to exit\n');
+  }
+});
+
+process.on('SIGINT', function () {
+  process.exit();
+});
+
+// rl.on('SIGINT', () => { 
+//   process.exit();
+// });
+
+process.on('exit', () => stdout.write('\nGL & HF!!!'));
